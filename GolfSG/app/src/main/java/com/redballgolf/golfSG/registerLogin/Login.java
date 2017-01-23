c170f1d07313fb44051c02af8a088c893ed867bc
Sean McAteer <seanmcateer100@hotmail.com>
2017-01-23 13:02:55 +0000

    Removed Main activity and creatd new packages

---
+++ GolfSG/app/src/main/java/com/redballgolf/golfSG/registerLogin/Login.java
@@ -0,0 +1,22 @@
+package com.redballgolf.golfSG.registerLogin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.sitting_room.golfsg.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }//onCreate
+
+}//Login class
